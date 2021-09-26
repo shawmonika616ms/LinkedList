dc1cb5d4386af5f593272357d2d5ac32bf7cc155
Monika Shaw <shawmonika616ms@gmail.com>
2021-09-26 17:26:47 +0530

    Adding elements in Singly LinkedList

---
+++ SinglyLinkedList.java
@@ -0,0 +1,35 @@
+public class SinglyLinkedList
+{
+	private ListNode head;
+	
+	private static class ListNode
+	{
+		private int data;
+		private ListNode next;
+		
+		public ListNode(int data)
+		{
+			this.data=data;
+			this.next=null;
+		}
+		
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		SinglyLinkedList sli= new SinglyLinkedList();
+		sli.head=new ListNode(10);
+		ListNode second= new ListNode(1);
+		ListNode third= new ListNode(8);
+		ListNode fourth= new ListNode(11);
+		
+		
+		sli.head.next=second;
+		second.next=third;
+		third.next=fourth;
+		
+	}
+	
+	
+}
