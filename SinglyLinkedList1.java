0f73b68c8692290da49fcbaa82701fd9e4cb5ee7
Monika Shaw <shawmonika616ms@gmail.com>
2021-09-27 18:13:06 +0530

    Represent a SinglyLinkedList

---
+++ SinglyLinkedList1.java
@@ -0,0 +1,18 @@
+package datastructure;
+
+public class SinglyLinkedList1 {
+
+	private ListNode head;
+	
+	private static class ListNode
+	{
+		private int data;
+		private ListNode next;
+		
+		public ListNode(int data)
+		{
+			this.data=data;
+			this.next=null;
+		}
+	}
+}
